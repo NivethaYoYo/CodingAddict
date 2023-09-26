384aa7e0650437a2ba45e1cd5adbd0367f4e4040
Nivetha75 <nivinach@gmail.com>
2023-09-26 18:32:17 +0530

    Ex 8 React.createElement()

--- exercises/src/index.js
+++ exercises/src/index.js
@@ -1,8 +1,20 @@
 import react from "react";
 import ReactDOM from "react-dom/client";
 
+/*function Greeting() {
+  return (
+    <div>
+      <h1>My First Component</h1>
+    </div>
+  );
+}*/
+
 function Greeting() {
-  return <h1>My First Component</h1>;
+  return react.createElement(
+    "div",
+    {},
+    react.createElement("h2", {}, "My First Component")
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
